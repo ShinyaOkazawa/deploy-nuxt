6e959b420a3b2e11194c52823a8da39da1255611
ShinyaOkazawa <fess3idm@gmail.com>
2024-09-12 16:59:34 +0900

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-console.log('processEnv: ' + process.env.APP_ENV)
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
